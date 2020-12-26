95f892a41eb7eced1d1952be2029e2877e05f1a1
Muhammet Mustafa Doğan <71767432+mumudgn@users.noreply.github.com>
2020-12-26 15:14:21 +0300

    Update Student_Management_System.py

--- Student_Management_System.py
+++ Student_Management_System.py
@@ -1,42 +1,55 @@
 check = False;lesson_list = [] ; try_signIn = 3 
-name = "muhammet mustafa doğan"
-results = {"midterm":38,"final":66,"project":89}
+name = "Muhammet Mustafa Doğan"
 def calculateGrade():
-    mean = (results["midterm"]*30 + results["final"]*50 + results["project"]*20)/100
+    d = {}
+    keys= ["midterm","final","project"]
+    for i in range(len(keys)):
+        values = int(input(f"Enter {keys[i]} note: ")) 
+        key = keys[i]
+        d[key] = values
+    print('-'*20)
+    mean = (d["midterm"]*30 + d["final"]*50 + d["project"]*20)/100
     return mean
-
+def lesson_check():
+    if len(lesson_list) < 3:
+        return "You are failed in class."
 while range(3):
     user = input("Please enter your name: ")
-    if user.lower() == name:
+    if user.lower() == name.lower():
         print(f"Welcome {user.upper()}")
+        print("Press q for complete after choosing lessons.")
         for i in range(5):
-            ask = input("Press q for complete or enter any key to continue : ")
             i += 1
-            if ask == "q":
-                break
-            lessons = input(f"Please choose lesson {i} : ")
+            lessons = input(f"Please addding lesson(s) {i} : ")
+            if lessons == "q":
+                print(lesson_check())
+                break 
             lesson_list.append(lessons)
-            check = True
-    
+        check = True
         break
     else:
         try_signIn -= 1
         if try_signIn == 0:
             print("Plese try again later.")
             break
-if len(lesson_list) < 3:
-    print("You are failed in class.")
-
 if check == True:
     print("".center(20,"-"))
-    print("Please select lesson for calculate your grade")
-    print(f"Your lessons\n1-{lesson_list[0]}\n2-{lesson_list[1]}\n3-{lesson_list[2]}\n4-{lesson_list[3]}\n5-{lesson_list[4]}")
-    lessonNum = int(input("Choose your lesson number:"))
+    print("Please select lesson for calculate your grade\nYour lessons")
+    for i in range(len(lesson_list)):
+        print(f"{i+1}-{lesson_list[i]}")
+    while True:
+        lessonNum = int(input("Choose your lesson number:"))
+        print('-'*20)
+        if lessonNum > len(lesson_list) or lessonNum <= 0:
+            print(f"You have {len(lesson_list)} lessons.Please enter a valid number.")
+        else:
+            break
     while True:    
         if lessonNum < 1 and lessonNum > 5:
             print("You must enter number of list")
         else:
             grade = calculateGrade()
+            print('-'*20)
             if grade >= 90:
                 print(f"Your grade is {grade}:AA ")
             elif grade >= 70 and grade < 90:
@@ -48,4 +61,3 @@ if check == True:
             elif grade < 30:
                 print(f"Your grade is {grade}:FF\nYou did not pass this lesson.")
         break
-        
